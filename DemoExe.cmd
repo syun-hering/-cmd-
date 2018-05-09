a93c198b9d8048fd1e4a514c2b094e7fa3069eb4
syun-hering <39107370+syun-hering@users.noreply.github.com>
2018-05-09 11:33:20 +0900

    Add files via upload
    OpenPoseのデモを動かすcmdファイルです。
    座標データと処理後の画像を保存するディレクトリを作ってあげてください

---
+++ DemoExe.cmd
@@ -0,0 +1,4 @@
+@echo off
+cd C:\OpenPose_demo_1.0.1\
+.\bin\OpenPoseDemo.exe --image_dir .\examples\test_image --write_images .\examples\test_image\PhotoData -write_keypoint .\examples\test_image\PointData
+cd C:\syun
